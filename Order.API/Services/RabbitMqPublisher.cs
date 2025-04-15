1b79e34843dc03d3f4764bd473f1e0d6d3a0d570
Mateus Gabriel <mateus92.mgs@gmail.com>
2025-04-15 06:24:26 -0300

    feat: criando classes

---
+++ Order.API/Services/RabbitMqPublisher.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Text;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Order.Core.Interfaces;
+using RabbitMQ.Client;
+
+namespace Order.API.Services
+{
+    public class RabbitMqPublisher : IMessagePublisher
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public RabbitMqPublisher()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" }; // depois ajustamos para docker
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public void Publish<T>(T message, string queueName)
+        {
+            _channel.QueueDeclare(queue: queueName,
+                                  durable: false,
+                                  exclusive: false,
+                                  autoDelete: false,
+                                  arguments: null);
+
+            var json = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            _channel.BasicPublish(exchange: "",
+                                  routingKey: queueName,
+                                  basicProperties: null,
+                                  body: body);
+        }
+    }
+}
---
+++ Order.Core/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Core.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string CustomerName { get; set; }
+        public List<string> Itens { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
